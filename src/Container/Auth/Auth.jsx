569d791d41e5f86225ec6910d0c4670a5ca0d7ec
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-11-26 00:08:59 +0530

    Jaihind

--- src/Container/Auth/Auth.jsx
+++ src/Container/Auth/Auth.jsx
@@ -28,6 +28,7 @@ const Auth = () => {
   const [ok, setOk] = useState(false);
   // const [state, setState] = useState({ isSignedIn: false });
   const con = useSelector((state) => state.isSignedIn);
+  console.log(con);
   const role = useSelector((state) => state.member);
 
   const dispatch = useDispatch();
@@ -37,9 +38,9 @@ const Auth = () => {
   const [showModal, setShowModal] = useState(false);
 
   const hideHandler = () => {
-    setShowModal(false)
-  }
-  
+    setShowModal(false);
+  };
+
   const functionover1 = () => {
     setIcon1(true);
   };
@@ -62,7 +63,6 @@ const Auth = () => {
 
   try {
     useEffect(() => {
-      
       const uiConfig = {
         signInFlow: "popup",
         signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
@@ -72,12 +72,9 @@ const Auth = () => {
       };
 
       firebase.auth().onAuthStateChanged((user) => {
-
         if (user) {
-          dispatch(Authactions.toggle());
           console.log("user", user);
           if (user.emailVerified) {
-
             localStorage.setItem("token", user.Aa);
 
             if (
@@ -85,7 +82,8 @@ const Auth = () => {
               user.email.includes("@iiitv.ac.in")
             ) {
               setOk(true);
-
+              dispatch(Authactions.toggle());
+              console.log("heey");
               if (user.email.includes("201951073"))
                 dispatch(Authactions.allocation("Registrar"));
               else if (user.email.includes("201951054"))
@@ -94,12 +92,8 @@ const Auth = () => {
                 dispatch(Authactions.allocation("Student"));
               else if (user.email.includes("201951073"))
                 dispatch(Authactions.allocation("Faculty"));
-
-              
-            }
-
-            else{
-              // setShowModal(true);
+            } else {
+              setShowModal(true);
               console.log("!!!!!!!");
             }
           }
@@ -113,7 +107,9 @@ const Auth = () => {
 
   return (
     <div className={styles.home}>
-      <Modal show={showModal} switch={hideHandler}>Please use your institute email ti login.</Modal>
+      <Modal show={showModal} switch={hideHandler}>
+        Please use your institute email ti login.
+      </Modal>
       {con ? (
         <span>
           <Home />
@@ -140,15 +136,28 @@ const Auth = () => {
           <div className={styles.div1}>
             <div className={styles.div2}>
               <div className={styles.div4}>HELLO</div>
-              <div className={styles.SignInButton1}>
-                <StyledFirebaseAuth
-                  uiConfig={uiConfig}
-                  firebaseAuth={firebase.auth()}
-                  id="google"
-                  className={styles.google}
-                  buttonText=""
-                />
-              </div>
+              {con ? (
+                <div className={styles.SignInButton1}>
+                  <StyledFirebaseAuth
+                    uiConfig={uiConfig}
+                    firebaseAuth={firebase.auth()}
+                    id="google"
+                    className={styles.google}
+                    buttonText=""
+                  />
+                </div>
+              ) : (
+                <div className={styles.SignInButton1}>
+                  <StyledFirebaseAuth
+                    uiConfig={uiConfig}
+                    firebaseAuth={firebase.auth()}
+                    id="google"
+                    className={styles.google}
+                    buttonText=""
+                  />
+                </div>
+              )}
+
               <div className={styles.div5}>
                 <a href="#">Trouble Logging In ?</a>
               </div>
